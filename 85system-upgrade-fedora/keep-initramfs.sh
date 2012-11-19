9ea26cb77fa5a2c3084959a2d51fcc7c37627f50
Will Woods <wwoods@redhat.com>
2012-11-19 18:06:42 -0500

    Make kernel modules available in old root
    We might still need to load kernel modules in the old root - for
    example, if we need to mount /boot/efi, we'll need vfat, which might not
    already be loaded.
    
    This patch bind-mounts /lib/modules/$(uname -r) into the target system
    so we can find and install them as needed.

--- 85system-upgrade-fedora/keep-initramfs.sh
+++ 85system-upgrade-fedora/keep-initramfs.sh
@@ -7,7 +7,17 @@ upgradedir="${NEWROOT}${UPGRADEROOT}"
 [ -n "$UPGRADEROOT" ] || die "UPGRADEROOT is unset, can't save initramfs"
 [ -d "$upgradedir" ] || die "'$upgradedir' doesn't exist"
 
+echo "saving initramfs to $upgradedir"
+
 mount -t tmpfs -o mode=755 tmpfs "$upgradedir" \
     || die "Can't mount tmpfs for $upgradedir"
 
 cp -ax / "$upgradedir" || die "failed to save initramfs to $upgradedir"
+
+moddir=lib/modules/$(uname -r)
+echo "making /$moddir available inside $NEWROOT"
+mount -o remount,rw $NEWROOT
+mkdir -p $NEWROOT/$moddir
+mount -o remount $NEWROOT
+mount --bind $upgradedir/$moddir $NEWROOT/$moddir \
+    || warn "failed to bind /$moddir into $NEWROOT"
