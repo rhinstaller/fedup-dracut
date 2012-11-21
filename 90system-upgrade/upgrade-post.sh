a6b643adeeb002b43b7e7c8beffb7102abfb789f
Will Woods <wwoods@redhat.com>
2012-11-21 17:37:27 -0500

    Revert "Absolutely awful hack to sync data before reboot"
    The underlying problem should be fixed by the following systemd commits:
    
      http://cgit.freedesktop.org/systemd/systemd/commit/?id=0049f05
      http://cgit.freedesktop.org/systemd/systemd/commit/?id=93bd157
      http://cgit.freedesktop.org/systemd/systemd/commit/?id=891a491
    
    This reverts commit 3da014f9fe5adc8d6d18675e8b231ed3146ecba8 with
    extreme prejudice and/or malice aforethought.

--- 90system-upgrade/upgrade-post.sh
+++ 90system-upgrade/upgrade-post.sh
@@ -15,20 +15,4 @@ getarg 'rd.upgrade.break=post' 'rd.break=upgrade-post' && \
     emergency_shell -n upgrade-post "Break before upgrade-post hook"
 source_hook upgrade-post
 
-# absolutely godawful hack workaround garbage that I am ashamed of:
-# Since we can't figure out how to unmount the root device properly
-# (it's inaccessable because we moved system-upgrade-root on top of it)
-# we need some other way to make sure all the data that might be pending
-# actually gets written to disk. So...
-
-startmsg="upgrade-post pid $$ doing emergency sync"
-echo 1 > /proc/sys/kernel/sysrq  # enable sysrq
-echo "$startmsg" > /dev/kmsg     # add marker to dmesg
-echo s > /proc/sysrq-trigger     # start sync
-for ((i=0; i<20; i++)); do       # wait up to 20s for sync to finish
-    dmesg | sed "1,/$startmsg/d" | grep -q 'Emergency Sync complete' && break
-    sleep 1
-done
-sleep 2                          # sleep a little more just to be sure
-
 exit 0
