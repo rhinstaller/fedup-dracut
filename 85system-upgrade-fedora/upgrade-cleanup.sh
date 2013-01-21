41ee6345000f2a379afd06023c5927a147bae4ac
Will Woods <wwoods@redhat.com>
2013-01-20 21:58:35 -0600

    Synergy.
    Maximize feature parity and all-beef synergies between graphical and
    text mode. This helps align us with industry best practices and
    subject matter expert recommendations, while creating new opportunities
    to grow our brand and increase recognition across key demographics.

--- 85system-upgrade-fedora/upgrade-cleanup.sh
+++ 85system-upgrade-fedora/upgrade-cleanup.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
 getargbool 0 rd.upgrade.test && return # skip cleanup if we were just testing
 chroot $NEWROOT fedup-cli --clean
+cat << '__BEEFYMIRACLE__'
+               .---. __
+    ,         /     \   \    ||||
+   \\\\      |O___O |    | \\||||
+   \   //    | \_/  |    |  \   /
+    '--/----/|     /     |   |-'
+           // //  /     -----'
+          //  \\ /      /
+         //  // /      /
+        //  \\ /      /
+       //  // /      /
+      /|   ' /      /
+      //\___/      /
+     //   ||\     /
+     \\_  || '---'
+     /' /  \\_.-
+    /  /    --| |
+    '-'      |  |
+              '-'
+__BEEFYMIRACLE__
