8d1f1a454d2439e96cb17fe819f10249169260e8
Will Woods <wwoods@redhat.com>
2013-10-23 17:46:21 -0400

    upgrade-cleanup: call 'fedup', not 'fedup-cli'
    The 'fedup-cli' name is deprecated. Use the right one.

--- 85system-upgrade-fedora/upgrade-cleanup.sh
+++ 85system-upgrade-fedora/upgrade-cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 getargbool 0 rd.upgrade.test && return # skip cleanup if we were just testing
 [ "$UPGRADE_STATE" == failed ] && return # skip cleanup on failure
-chroot $NEWROOT fedup-cli --clean
+chroot $NEWROOT fedup --clean
 cat << '__BEEFYMIRACLE__'
 |               .---. __
 |    ,         /     \   \    ||||
