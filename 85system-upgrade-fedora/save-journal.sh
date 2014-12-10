660157b2d08a5c9d95b8ea1e6eaddb2603ef0983
Will Woods <wwoods@redhat.com>
2014-12-10 17:42:34 -0500

    save-journal.sh: add boot ID to log, remove -m
    Saving the boot ID gives you a way to pull the upgrade log data directly
    from the journal, if you want to search / filter / get more data / etc.
    
    Also, remove the '-m' from the journalctl invocation, since we don't
    really want to pull remote data here.

--- 85system-upgrade-fedora/save-journal.sh
+++ 85system-upgrade-fedora/save-journal.sh
@@ -8,5 +8,7 @@ logfile=/sysroot/var/log/upgrade.log
 # back up old logfile, if present
 [ -e $logfile ] && rm -rf ${logfile}.old && mv $logfile ${logfile}.old
 
-# write out the logfile. bam!
-journalctl -b -a -m > $logfile
+# write out the logfile
+bootid=$(</proc/sys/kernel/random/boot_id); bootid=${bootid//-/}
+echo "-- Boot ID: $bootid --" > $logfile
+journalctl -b -a >> $logfile
